916c5ed04b31e792e95618667e4e7bca6d5c38d7
SixtineNodet <sixtine.nodet@gmail.com>
2024-04-24 20:56:26 +0200

    add movie card with style, image, title (issue stars)

--- src/components/MovieCatalog/MovieCatalog.tsx
+++ src/components/MovieCatalog/MovieCatalog.tsx
@@ -2,11 +2,17 @@ import styles from "./MovieCatalog.module.css";
 
 import GenreList from "../GenreList/GenreList";
 
+import MoviePreview from "../MoviePreview/MoviePreview"
+import { mockMovie } from "../../mock/mockMovie";
+
 export const MovieCatalog = () => {
   return (
     <>
       <GenreList />
-      <div className={styles.layout}></div>
+      <div className={styles.layout}>
+        <MoviePreview movie={mockMovie} ></MoviePreview>
+      </div>
+      
     </>
   );
 };
--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -1,11 +1,29 @@
+import { POSTER_URL_PREFIX } from "../../App";
+import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
-const MoviePreview = () => {
+const MovieStars = ({ score }: { score: number }) => {
+  return <div className={styles.stars}>
+    {[...Array(5)].map((_, index) => {
+      return <div className={`${index <= score/2 ? styles.yellow : styles.black
+        }`}> ★</div>
+    })}
+  </div>
+}
+
+
+const MoviePreview = ({ movie }: { movie: Movie }) => {
   const goToMovieDetails = () => {
     // A DEFINIR EXPERT
   };
 
-  return <div className={styles.movieCard} onClick={goToMovieDetails}></div>;
+  return <div className={styles.movieCard} onClick={goToMovieDetails}>
+    <img
+      className={styles.moviePicture}
+      src={POSTER_URL_PREFIX + movie.poster_path} />
+    {movie.title}
+    <MovieStars score={movie.vote_average / 2} />
+  </div>;
 };
 
 export default MoviePreview;
